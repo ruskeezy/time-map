749d217b2b2bec6b3fe82d67eccab8a38dc54cf4
ruskeezy <christian.ax.miller@gmail.com>
2018-01-03 09:45:28 -0800

    changed HTML/DOM, everything works

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 'use strict';
 
+// grabbing html elements
+
+var run = document.getElementById('running');
+var lift = document.getElementById('lifting');
+var cycle = document.getElementById('cycling');
+var tv = document.getElementById('tv');
+var read = document.getElementById('reading');
+var music = document.getElementById('music');
+var meeting = document.getElementById('meetings');
+var project = document.getElementById('projects');
+var program = document.getElementById('programming')
+
+// global variables
 var activities = [];
 var startTime;
 var stopTime;
 var ul = document.getElementById('activity-list');
 var selectedActivity;
 
+// start function, connected to start button
 function start() {
   startTime = Date.now();
 }
 
+// stop function, connected to stop button. converts to seconds
 function stop(){
   stopTime = Date.now();
   console.log(selectedActivity.total += ((stopTime - startTime) / 1000));
@@ -17,33 +32,62 @@ function stop(){
   stopTime = null;
 }
 
+// constructor function
 function Activity (name) {
   this.name = name;
   this.total = 0;
   activities.push(this);
 }
 
-new Activity('TV');
+// instantiation
 new Activity('Running');
 new Activity('Lifting');
-new Activity('Music');
+new Activity('Cycling')
+new Activity('TV');
 new Activity('Reading');
+new Activity('Music');
+new Activity('Meetings');
+new Activity('Projects');
+new Activity('Programming');
 
-// for(var i = 0; i < activities.length; i ++){
-//   (function() {
-//       var activity = activities[i];
-//       var liEl = document.createElement('li');
-//       var aEl = document.createElement('a');
-//       aEl.innerText = activities[i].name;
-//       liEl.appendChild(aEl);
-//       liEl.addEventListener('click', function() {
-//         selectedActivity = activity;
-//       })
-//       ul.appendChild(liEl);
-//   }())
-// }
+// click handlers
+run.addEventListener('click', function(){
+  selectedActivity = activities[0]
+  console.log(selectedActivity);
+})
+lift.addEventListener('click', function(){
+  selectedActivity = activities[1]
+  console.log(selectedActivity);
+})
+cycle.addEventListener('click', function(){
+  selectedActivity = activities[2]
+  console.log(selectedActivity);
+})
+tv.addEventListener('click', function(){
+  selectedActivity = activities[3]
+  console.log(selectedActivity);
+})
+read.addEventListener('click', function(){
+  selectedActivity = activities[4]
+  console.log(selectedActivity);
+})
+music.addEventListener('click', function(){
+  selectedActivity = activities[5]
+  console.log(selectedActivity);
+})
+meeting.addEventListener('click', function(){
+  selectedActivity = activities[6]
+  console.log(selectedActivity);
+})
+project.addEventListener('click', function(){
+  selectedActivity = activities[7]
+  console.log(selectedActivity);
+})
+program.addEventListener('click', function(){
+  selectedActivity = activities[8]
+  console.log(selectedActivity);
+})
 
-// add click handlers to start and stop button
 
 function chartMaker() {
   var chartPlace = document.getElementById('my-chart')
@@ -71,3 +115,18 @@ function chartMaker() {
 }
 
 chartMaker();
+
+
+// for(var i = 0; i < activities.length; i ++){
+//   (function() {
+//       var activity = activities[i];
+//       var liEl = document.createElement('li');
+//       var aEl = document.createElement('a');
+//       aEl.innerText = activities[i].name;
+//       liEl.appendChild(aEl);
+//       liEl.addEventListener('click', function() {
+//         selectedActivity = activity;
+//       })
+//       ul.appendChild(liEl);
+//   }())
+// }
